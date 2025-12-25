ca688bf1accda929a8968de00160adc586098f86
espoma <espoma@protonmail.ch>
2025-12-25 20:38:17 +0530

    feat: added test for transform method

---
+++ tests/test_transform.py
@@ -0,0 +1,29 @@
+import unittest
+import numpy as np
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from dspca import DSPCA
+
+class TestDSPCATransform(unittest.TestCase):
+    def setUp(self):
+        self.X = np.random.randn(10, 10)
+        self.dspca = DSPCA(n_components=2, sparsity_levels=[5, 3], max_sensors=8)
+        self.dspca.fit(self.X)
+
+    def test_transform_shape(self):
+        X_transformed = self.dspca.transform(self.X)
+        self.assertEqual(X_transformed.shape, (10, 2))
+
+    def test_transform_errors(self):
+        unfitted = DSPCA(n_components=1, sparsity_levels=[1], max_sensors=1)
+        with self.assertRaises(ValueError):
+            unfitted.transform(self.X)
+        
+        X_wrong = np.random.randn(10, 5)
+        with self.assertRaises(ValueError):
+            self.dspca.transform(X_wrong)
+
+if __name__ == '__main__':
+    unittest.main()
